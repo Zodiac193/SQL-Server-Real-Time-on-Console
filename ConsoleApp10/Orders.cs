9411546e5ccf512263bb582eaa39dc9ca6902e43
Mohamed Refaat <medom193@gmail.com>
2022-10-01 18:05:36 +0200

    Add project files.

---
+++ ConsoleApp10/Orders.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp10
+{
+     class Orders
+    {
+        public int id { get; set; }
+        public string Status { get; set; }
+
+    }
+}
---
+++ ConsoleApp10/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using TableDependency.SqlClient;
+using TableDependency.SqlClient.Base.Enums;
+
+namespace ConsoleApp10
+{
+     class Program
+    {
+        public static void Main(string[] args)
+        {
+            string connection = @"Data Source=DESKTOP-LBIQ3KN\SQLEXPRESS; Initial Catalog=new_db; User id = sa1 ; password = 123456;";
+
+             using (var tbDependancy = new SqlTableDependency<Orders>(connection) )
+            {
+                tbDependancy.OnChanged += TbDependancy_OnChanged;
+                tbDependancy.Start();
+
+                Console.ReadKey();
+            }
+        }
+
+        private static void TbDependancy_OnChanged(object sender, TableDependency.SqlClient.Base.EventArgs.RecordChangedEventArgs<Orders> e)
+        {
+            //throw new NotImplementedException();
+
+            if(e.ChangeType != ChangeType.None)
+            {
+                var Entry = e.Entity;
+                Console.WriteLine("Type : " + e.ChangeType );
+                Console.WriteLine("ID : " + Entry.id + "\n" +"Status :" + Entry.Status);
+            }
+        }
+    }
+}
